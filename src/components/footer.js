01659e101822b09caec5bb808555ccacb587f19d
a14a19 <anas31197@gmail.com>
2022-11-16 21:47:18 +0530

    Initial Commit

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import classes from './footer.module.css';
+
+function Footer() {
+    return (
+        <>
+            <div>Footer components</div>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import classes from './header.module.css';
+
+function Header() {
+    return (
+        <>
+            <div>Header components</div>
+        </>
+    )
+}
+
+export default Header;
